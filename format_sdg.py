fe3bfedaa6506fcab84e420897509e14468f798a
marcelarodriguez <rodrmd1419@gmail.com>
2020-06-22 14:36:21 -0500

    comments corrections

--- format_sdg.py
+++ format_sdg.py
@@ -7,9 +7,7 @@ import rpy2
 import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
 
-base = importr('base')
 utils = importr('utils')
-stats = importr('stats')
 
 utils.install_packages('neonUtilities', repos='https://cran.rstudio.com/');
 neonUtilities = importr('neonUtilities')
